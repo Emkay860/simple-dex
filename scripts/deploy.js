eabb6a01fc329eafdec89d84753cb556be603b00
Majid Kareem <kareemmajid86@gmail.com>
2022-07-30 04:31:21 +0100

    remove unrelated files

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,20 +8,6 @@
 const { ethers } = require('hardhat');
 
 async function main() {
-	// const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-	// const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-	// const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-	// const lockedAmount = hre.ethers.utils.parseEther("1");
-
-	// const Lock = await hre.ethers.getContractFactory("Lock");
-	// const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-	// await lock.deployed();
-
-	// console.log("Lock with 1 ETH deployed to:", lock.address);
-
-	// Deploy token1 contract
 	const Token1 = await ethers.getContractFactory('Token1');
 	const token1 = await Token1.deploy();
 
